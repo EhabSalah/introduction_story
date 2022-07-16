08616ed75007cfa5c14ac54ea7a5ec530dfec626
Ehab Salah <ehab.salah020@gmail.com>
2022-07-16 20:19:00 +0100

    Initial commit 🌱

---
+++ example/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Introduction Story',
+      theme: ThemeData.light(),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Introduction Story'),
+      ),
+    );
+  }
+}
