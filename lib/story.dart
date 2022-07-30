171d692fa90dcad586429852678f7e81064c531f
Ehab Salah <ehab.salah020@gmail.com>
2022-07-31 00:01:25 +0200

    story.dart: add foreground widget and theme mode

--- lib/story.dart
+++ lib/story.dart
@@ -1,15 +1,38 @@
-import 'dart:ui';
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 class Story {
-  final String title;
-  final String description;
   final Color? backgroundColor;
   final String? imagePath;
+  final _StoryTheme storyThemeMode;
+  final Widget? foreground;
 
   Story({
-    required this.title,
-    required this.description,
     this.backgroundColor,
     this.imagePath,
-  }) : assert(imagePath != null || backgroundColor != null);
+    this.foreground,
+    bool lightMode = true,
+  })  : assert(imagePath != null || backgroundColor != null,
+            'imagePath or backgroundColor must be set, to be used as a background'),
+        storyThemeMode = _StoryTheme(lightMode);
+}
+
+class _StoryTheme {
+  final Color foregroundColor;
+  final Brightness brightness;
+
+  _StoryTheme(bool lightTheme)
+      : foregroundColor =
+            lightTheme ? const Color(0xffffffff) : const Color(0xff000000),
+        brightness = lightTheme ? lightBrightness() : darkBrightness();
+
+  // For iOS.
+  // Use [dark] for white status bar and [light] for black status bar.
+  static lightBrightness() =>
+      Platform.isIOS ? Brightness.dark : Brightness.light;
+
+  static darkBrightness() =>
+      Platform.isIOS ? Brightness.light : Brightness.dark;
 }
