c1541c94ca25dcd34bcd4a52b7f1b196e2462215
Ehab Salah <ehab.salah020@gmail.com>
2022-08-13 19:31:00 +0100

    example: build the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:story_introduction/story_introduction.dart';
 
@@ -6,13 +7,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Story Introduction',
-      theme: ThemeData.light(),
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -29,60 +30,94 @@ class HomePage extends StatelessWidget {
         title: const Text('Story Introduction'),
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Text(
-                'Press to introduce new thing to user 👀',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(14),
+              child: Text.rich(
+                TextSpan(
+                  text:
+                      'We can suppose that this is a brief for the newly provided feature. Checkout the CTA below.\n',
+                  children: [
+                    TextSpan(
+                      text: 'Tap for more information about $_featureName. 🍇',
+                      style: const TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: Colors.lightBlueAccent,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () => _pushIntroductionStoriesScreen(context),
+                    )
+                  ],
+                ),
               ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                      opaque: false,
-                      pageBuilder: (_, __, ___) => StoryIntroductionScreen(
-                        StoryIntroductionProps(
-                          featureName: 'New Feature Name',
-                          duration: 4000,
-                          isDismissible: true,
-                          stories: [
-                            Story(
-                              title: 'React',
-                              backgroundColor: Colors.red,
-                              description: 'Description',
-                            ),
-                            Story(
-                              title: 'React',
-                              backgroundColor: Colors.green,
-                              description: 'Description',
-                            ),
-                            Story(
-                              title: 'React',
-                              backgroundColor: Colors.blue,
-                              description: 'Description',
-                            ),
-                            Story(
-                              title: 'React',
-                              backgroundColor: Colors.orange,
-                              description: 'Description',
-                            ),
-                          ],
-                        ),
+            ),
+            Expanded(
+              child: ListView.separated(
+                padding: const EdgeInsets.all(12),
+                separatorBuilder: (_, __) => const SizedBox(height: 12),
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200]?.withOpacity(.1),
+                      borderRadius: BorderRadius.circular(20),
+                      image: const DecorationImage(
+                        image: AssetImage('assets/placeholder.png'),
                       ),
                     ),
                   );
                 },
-                child: const Text('New feature ✨'),
+                itemCount: 5,
               ),
-            ],
-          ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String get _featureName => 'Grocery Store';
+
+  void _pushIntroductionStoriesScreen(BuildContext context) {
+    Navigator.push<void>(
+      context,
+      PageRouteBuilder(
+        opaque: false,
+        pageBuilder: (_, __, ___) => StoryIntroductionScreen(
+          isDismissible: true,
+          stories: [
+            Story(
+              imagePath: 'assets/image_1.png',
+              featureName: _featureName,
+              title: 'Online Grocery Shopping',
+              description:
+                  'Shop your favourite groceries and ready to cook, heat & '
+                  'eat selection of artisanal, handcrafted products made '
+                  'fresh daily for you to devour.',
+            ),
+            Story(
+              imagePath: 'assets/image_2.png',
+              title: 'Premium quality is our aim',
+              featureName: _featureName,
+              description:
+                  'Categories: Fresh Fruits, Vegetables & Herbs, Bakery & '
+                  'Pastry, Cheese, Dairy & Deli, Desserts & Sweets.',
+              decoration: const StoryDecoration(lightMode: false),
+            ),
+            Story(
+              imagePath: 'assets/image_3.png',
+              title: 'We deliver fresh and fast',
+              featureName: _featureName,
+              description:
+                  'Your gourmet groceries are just a click away! Pick your '
+                  'favourite groceries and receive your order '
+                  'within 15 minutes.',
+              decoration: const StoryDecoration(lightMode: false),
+            ),
+          ],
         ),
       ),
     );
